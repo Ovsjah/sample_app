81213f2638e7e4848ccace586e2d11dcbb3b76cb
Ovsjah <ovsjah@gmail.com>
2019-10-18 04:23:13 +0300

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,1 @@
-module StaticPagesHelper
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    page_title.present? ? "#{page_title} | #{base_title}" : base_title
-  end
-end
+module StaticPagesHelper; end
